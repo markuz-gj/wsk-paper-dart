83a341f6042cdbcebb31c67faa38275084c0a5c5
Markuz GJ <markuz.br@gmail.com>
2014-08-13 15:27:14 -0700

    saving work

---
+++ app/index.dart
@@ -0,0 +1,15 @@
+// import 'package:polymer/polymer.dart';
+
+/**
+ * A Polymer click counter element.
+ */
+
+main () {
+  print('index.dart');
+}
+
+/*
+*
+must work on the build process now.
+*
+*/
