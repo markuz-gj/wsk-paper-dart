5c0679a69768fe874e78565f4cbe33b1813044fa
Markuz GJ <markuz.br@gmail.com>
2014-08-14 03:32:09 -0700

    mostly done with basic build

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,47 @@
-// /**
-//  *
-//  *  Web Starter Kit
-//  *  Copyright 2014 Google Inc. All rights reserved.
-//  *
-//  *  Licensed under the Apache License, Version 2.0 (the "License");
-//  *  you may not use this file except in compliance with the License.
-//  *  You may obtain a copy of the License at
-//  *
-//  *      http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  *  Unless required by applicable law or agreed to in writing, software
-//  *  distributed under the License is distributed on an "AS IS" BASIS,
-//  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  *  See the License for the specific language governing permissions and
-//  *  limitations under the License
-//  *
-//  */
+/**
+ *
+ *  Web Starter Kit
+ *  Copyright 2014 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
 
-// 'use strict';
+'use strict';
+var exec = require('child_process').exec
 
-// // Include Gulp & Tools We'll Use
-// var gulp = require('gulp');
-// var $ = require('gulp-load-plugins')();
-// var del = require('del');
-// var runSequence = require('run-sequence');
-// var browserSync = require('browser-sync');
-// var pagespeed = require('psi');
-// var reload = browserSync.reload;
+// Include Gulp & Tools We'll Use
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var pagespeed = require('psi');
+var reload = browserSync.reload;
 
-// var AUTOPREFIXER_BROWSERS = [
-//   'ie >= 10',
-//   'ie_mob >= 10',
-//   'ff >= 30',
-//   'chrome >= 34',
-//   'safari >= 7',
-//   'opera >= 23',
-//   'ios >= 7',
-//   'android >= 4.4',
-//   'bb >= 10'
-// ];
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
 
+// // TODO: write the equivalent of dart
 // // Lint JavaScript
 // gulp.task('jshint', function () {
 //   return gulp.src('app/scripts/**/*.js')
@@ -60,18 +62,6 @@
 //     .pipe($.size({title: 'images'}));
 // });
 
-// // Copy All Files At The Root Level (app)
-// gulp.task('copy', function () {
-//   return gulp.src([
-//     'app/*',
-//     '!app/*.html',
-//     'node_modules/apache-server-configs/dist/.htaccess'
-//   ], {
-//     dot: true
-//   }).pipe(gulp.dest('dist'))
-//     .pipe($.size({title: 'copy'}));
-// });
-
 // // Copy Web Fonts To Dist
 // gulp.task('fonts', function () {
 //   return gulp.src(['app/fonts/**'])
@@ -79,115 +69,141 @@
 //     .pipe($.size({title: 'fonts'}));
 // });
 
-// // Compile and Automatically Prefix Stylesheets
-// gulp.task('styles', function () {
-//   // For best performance, don't add Sass partials to `gulp.src`
-//   return gulp.src([
-//       'app/styles/*.scss',
-//       'app/styles/**/*.css',
-//       'app/styles/components/components.scss'
-//     ])
-//     .pipe($.changed('.tmp/styles', {extension: '.css'}))
-//     .pipe($.if('*.scss', $.rubySass({
-//       style: 'expanded',
-//       precision: 10
-//     })
-//     .on('error', console.error.bind(console))
-//     ))
-//     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-//     // Remove Any Unused CSS
-//     // Note: If not using the Style Guide, you can delete it from
-//     // the next line to only include styles your project uses.
-//     .pipe($.if('*.css', $.uncss({
-//       html: [
-//         'app/index.html',
-//         'app/styleguide/index.html'
-//       ],
-//       // CSS Selectors for UnCSS to ignore
-//       ignore: [
-//         /.navdrawer-container.open/,
-//         /.app-bar.open/
-//       ]
-//     })))
-//     .pipe(gulp.dest('.tmp/styles'))
-//     .pipe($.size({title: 'styles'}));
-// });
+// Copy All Files At The Dart Root Level (web)
+gulp.task('copy', function(){
+  return gulp.src([
+    'app/**/*.{html,js,dart,map}',
+    '.tmp/**/*.{html,css,map}',
+    '!app/{packages,*/packages}/**'], {dot:true})
+  .pipe(gulp.dest('web'))
+  .pipe($.size({title: 'copy'}))
+})
 
-// // Scan Your HTML For Assets & Optimize Them
-// gulp.task('html', function () {
-//   var assets = $.useref.assets({searchPath: '{.tmp,app}'});
+// TODO: add comments
+gulp.task('minify', function(){
+  return gulp.src('build/**/*.{html,css,js,map}', {dot:true})
+    // Remove Any Unused CSS
+    // Note: If not using the Style Guide, you can delete it from
+    // the next line to only include styles your project uses.
+    .pipe($.if('*.css', $.uncss({
+      html: [
+        'build/web/index.html',
+        'build/web/click_counter/index.html'
+      ],
+      // CSS Selectors for UnCSS to ignore
+      ignore: []
+    })))
+    .pipe($.if('*.css', $.csso()))
+    .pipe($.if('*.html', $.htmlmin({
+        minifyCSS: true,
+        minifyJS: true,
+        collapseWhitespace: true,
+        removeComments:true,
+        removeAttributeQuotes: true
+    })))
+    // TODO: uglify crashing. RangeError: Maximum call stack size exceeded
+    // .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+    .pipe(gulp.dest('dist'))
+    .pipe($.size({title: 'minify'}))
+})
 
-//   return gulp.src('app/**/*.html')
-//     .pipe(assets)
-//     // Concatenate And Minify JavaScript
-//     .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
-//     // Concatenate And Minify Styles
-//     .pipe($.if('*.css', $.csso()))
-//     .pipe(assets.restore())
-//     .pipe($.useref())
-//     // Update Production Style Guide Paths
-//     .pipe($.replace('components/components.css', 'components/main.min.css'))
-//     // Minify Any HTML
-//     .pipe($.if('*.html', $.minifyHtml()))
-//     // Output Files
-//     .pipe(gulp.dest('dist'))
-//     .pipe($.size({title: 'html'}));
-// });
+// Compile and Automatically Prefix Stylesheets
+gulp.task('styles', function () {
+  // For best performance, don't add Sass partials to `gulp.src`
+  return gulp.src([
+      'app/**/*.{scss,css}',
+      '!app/{packages,*/packages}/**'
+    ])
+    .pipe($.if('*.scss', $.rubySass({
+      style: 'expanded',
+      precision: 10
+    })
+    .on('error', console.error.bind(console))
+    ))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('.tmp'))
+    .pipe($.size({title: 'styles'}));
+});
 
-// // Clean Output Directory
-// gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+// TODO: add comments
+gulp.task('jade', function(){
+  return gulp.src([
+      'app/**/*.{html,jade}',
+      '!app/{packages,*/packages}/**'
+    ])
+    .pipe($.if('*.jade', $.jade({pretty: true})))
+    .on('error', console.error.bind(console))
+    .pipe(gulp.dest('.tmp'))
+    .pipe($.size({title: 'jade'}))
+});
 
-// // Watch Files For Changes & Reload
-// gulp.task('serve', ['styles'], function () {
-//   browserSync({
-//     notify: false,
-//     // Run as an https by uncommenting 'https: true'
-//     // Note: this uses an unsigned certificate which on first access
-//     //       will present a certificate warning in the browser.
-//     // https: true,
-//     server: {
-//       baseDir: ['.tmp', 'app']
-//     }
-//   });
+// TODO: add comments
+gulp.task('assets', function(cb){
+  runSequence(['styles', 'jade'], cb)
+})
 
-//   gulp.watch(['app/**/*.html'], reload);
-//   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
-//   gulp.watch(['app/scripts/**/*.js'], ['jshint']);
-//   gulp.watch(['app/images/**/*'], reload);
-// });
+// Clean Output Directory
+gulp.task('clean', del.bind(null, ['web', 'build', '.tmp', 'dist']));
 
-// // Build and serve the output from the dist build
-// gulp.task('serve:dist', ['default'], function () {
-//   browserSync({
-//     notify: false,
-//     // Run as an https by uncommenting 'https: true'
-//     // Note: this uses an unsigned certificate which on first access
-//     //       will present a certificate warning in the browser.
-//     // https: true,
-//     server: {
-//       baseDir: 'dist'
-//     }
+// executing pub build
+gulp.task('build', function(cb){
+  var cmd = exec('pub build');
+  cmd.stdout.pipe(process.stdout);
+  cmd.stderr.pipe(process.stderr);
+  cmd.on('close', cb);
+})
 
-//   });
-// });
+// Watch Files For Changes & Reload
+gulp.task('serve', ['assets'], function () {
+  browserSync({
+    notify: false,
+    // browser: 'chromium',
+    browser: 'skip',
+    // forces full page reload on css changes.
+    injectChanges: false,
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: ['.tmp', 'app']
+    }
+  });
 
-// // Build Production Files, the Default Task
-// gulp.task('default', ['clean'], function (cb) {
-//   runSequence('styles', ['jshint', 'html', 'images', 'fonts', 'copy'], cb);
-// });
+  gulp.watch([
+    'app/**/*.{jade,html}',
+    '!app/{packages,*/packages}/**'], ['jade', reload]);
 
-// // Run PageSpeed Insights
-// // Update `url` below to the public URL for your site
-// gulp.task('pagespeed', pagespeed.bind(null, {
-//   // By default, we use the PageSpeed Insights
-//   // free (no API key) tier. You can use a Google
-//   // Developer API key if you have one. See
-//   // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-//   url: 'https://example.com',
-//   strategy: 'mobile'
-// }));
+  gulp.watch([
+    'app/**/*.{scss,css}',
+    '!app/{packages,*/packages}/**'], ['styles', reload]);
 
-// // Load custom tasks from the `tasks` directory
-try { require('require-dir')('tasks'); } catch (err) {
-  console.log(err.stack)
-}
+  gulp.watch([
+    'app/**/*.{dart,js}',
+    '!app/{packages,*/packages}/**'], reload);
+
+});
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['default'], function () {
+  browserSync({
+    notify: false,
+    // Run as an https by uncommenting 'https: true'
+    // Note: this uses an unsigned certificate which on first access
+    //       will present a certificate warning in the browser.
+    // https: true,
+    server: {
+      baseDir: 'dist/web'
+    }
+  });
+});
+
+// TODO: add comments
+gulp.task('default', ['clean'], function(cb){
+  runSequence('assets', 'copy', 'build', 'minify', cb)
+})
+
+// Load custom tasks from the `tasks` directory
+try { require('require-dir')('tasks'); } catch (err) {}
+
+
